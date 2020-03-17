a04cbc69734e99d94a9bb7e49af14de3af735bc3
Hugo Feijó <hugo.ofeijo@gmail.com>
2020-03-17 09:37:07 -0400

    Feat: Delete Lançamento

--- src/main/java/com/hugo/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/hugo/algamoney/api/resource/LancamentoResource.java
@@ -13,12 +13,14 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hugo.algamoney.api.event.RecursoCriadoEvent;
@@ -62,6 +64,12 @@ public class LancamentoResource {
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
 		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
 	}
+
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long codigo){
+		lancamentoRepository.deleteById(codigo);
+	}
 	
 	@ExceptionHandler(PessoaInativaException.class)
 	public ResponseEntity<Object> handlePessoaInativaException(PessoaInativaException ex){
@@ -71,4 +79,5 @@ public class LancamentoResource {
 		
 		return ResponseEntity.badRequest().body(erros);
 	}
+	
 }
