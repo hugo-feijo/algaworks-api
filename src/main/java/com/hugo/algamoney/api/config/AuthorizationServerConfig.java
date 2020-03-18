daaa2e895412298355665c4f72ce6de9cb9ee660
Hugo Feijó <hugo.ofeijo@gmail.com>
2020-03-17 20:40:15 -0400

    Refresh token implementado

--- src/main/java/com/hugo/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/hugo/algamoney/api/config/AuthorizationServerConfig.java
@@ -29,14 +29,16 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		clients.inMemory()
 			.withClient("angular").secret(encoder.encode("@ngul@r0"))
 			.scopes("read", "write")
-			.authorizedGrantTypes("password")
-			.accessTokenValiditySeconds(1800);
+			.authorizedGrantTypes("password", "refresh_token")
+			.accessTokenValiditySeconds(20)
+			.refreshTokenValiditySeconds(3600*24);
 	}
 	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints.tokenStore(tokenStore())
 		.accessTokenConverter(accessTokenConverter())
+		.reuseRefreshTokens(false)
 		.authenticationManager(authenticationManager);
 	}
 	
