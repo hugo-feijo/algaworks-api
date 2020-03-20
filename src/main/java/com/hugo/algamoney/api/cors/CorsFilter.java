879794be1c8116f51cd5d906ddaf7532b139edb7
Hugo Feijó <hugo.ofeijo@gmail.com>
2020-03-20 15:27:45 -0400

    Perfil de configuração do spring implementado

--- src/main/java/com/hugo/algamoney/api/cors/CorsFilter.java
+++ src/main/java/com/hugo/algamoney/api/cors/CorsFilter.java
@@ -10,15 +10,20 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import com.hugo.algamoney.api.config.property.AlgamoneyApiProperty;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter{
 
-	private String originPermitida = "http://localhost:8000";//TODO: Configurar para diferentes ambientes
+	@Autowired
+	private AlgamoneyApiProperty algamoneyApiProperty;
+	
 	
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
@@ -27,10 +32,10 @@ public class CorsFilter implements Filter{
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpServletResponse resp = (HttpServletResponse) response;
 		
-		resp.setHeader("Access-Control-Allow-Origin", originPermitida);
+		resp.setHeader("Access-Control-Allow-Origin", algamoneyApiProperty.getOrigemPermitida());
         resp.setHeader("Access-Control-Allow-Credentials", "true");
         
-		if("OPTIONS".equals(req.getMethod()) && originPermitida.equals(req.getHeader("Origin"))) {
+		if("OPTIONS".equals(req.getMethod()) && algamoneyApiProperty.getOrigemPermitida().equals(req.getHeader("Origin"))) {
 			resp.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
         	resp.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
         	resp.setHeader("Access-Control-Max-Age", "3600");
--- src/main/java/com/hugo/algamoney/api/resource/TokenResource.java
+++ src/main/java/com/hugo/algamoney/api/resource/TokenResource.java
@@ -4,20 +4,26 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hugo.algamoney.api.config.property.AlgamoneyApiProperty;
+
 @RestController
 @RequestMapping("/tokens")
 public class TokenResource {
 
+	@Autowired
+	private AlgamoneyApiProperty algamoneyApiProperty;
+	
 	@DeleteMapping("/revoke")
 	public void revoke(HttpServletRequest requeste, HttpServletResponse response) {
 		Cookie refreshTokenCookie = new Cookie("refreshToken", null);
 		refreshTokenCookie.setHttpOnly(true);
-		refreshTokenCookie.setSecure(true);//TODO: em produção será true
+		refreshTokenCookie.setSecure(algamoneyApiProperty.getSeguranca().isEnableHttps());
 		refreshTokenCookie.setPath(requeste.getContextPath() + "/oauth/token");
 		refreshTokenCookie.setMaxAge(0);
 		
