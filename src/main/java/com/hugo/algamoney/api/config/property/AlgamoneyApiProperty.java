da6d332837bca5d08cf8d36aef82d7a71bb0d6d6
Hugo Feijó <hugo.ofeijo@gmail.com>
2020-03-23 09:55:18 -0400

    Remove @Value in ApiProperty and Create Procfile for heroku

--- src/main/java/com/hugo/algamoney/api/config/property/AlgamoneyApiProperty.java
+++ src/main/java/com/hugo/algamoney/api/config/property/AlgamoneyApiProperty.java
@@ -1,12 +1,10 @@
 package com.hugo.algamoney.api.config.property;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix =  "algamoney")
 public class AlgamoneyApiProperty {
 
-	@Value("${origem-Permitida}")
 	private String origemPermitida = "http://localhost:8000";
 	
 	private final Seguranca seguranca = new Seguranca();
